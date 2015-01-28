da47b23874d604fcdd8ec0393c2fb9371495e82a
Alexander Close <alexander.close@gmail.com>
2015-01-27 22:15:38 -0800

    Setting up files

--- Readme.md
+++ Readme.md
@@ -3,14 +3,13 @@
 User will plug in Kindle Device to Mac
 
 Mac will detect the Kindle and download the file /documents/My Clippings.txt to /Users/"+username+"/Dropbox/kindle-clippings.txt and replace the file
+if one is previously present
 
-Mac will read the file and detect any changes
+Mac will read the file from the dropbox folder and detect any changes
 
 *first search for tags, and search for title - see if the content exists first, to avoid duplicate entries in a book
 
-The changes (new notes) will either be added to previously made Evernote notes and append those notes with added text
+File changes (new highlights) will be added to previously made Evernote notes - append those notes
 
-	or
-
-Create a new Evernote note for the book and add the new notes
+New highlights for new kindle books will create an evernote note within the evernote notebook "Kindle_Highlights"
 
---
+++ kindle_to_evernote_sync.py
@@ -0,0 +1,60 @@
+import os
+import time
+
+username = "alexander.close2"
+document = open("/Users/"+username+"/Dropbox/kindle-clippings.txt","r") 
+ 
+data = "".join(document.readlines())
+notes = []
+try:
+    clippings = data.split('==========')
+    for clip in clippings:
+        clipping = clip.split('Added on ')
+
+        title = clipping[0].split('\r\n- ')[0].replace('\r\n','')
+        date = clipping[1].split('\r\n')[0]
+        location = clipping[0].split('\r\n- ')[1].replace('\r\n','')
+        text = clipping[1].split('\r\n\r\n')[1]
+        note = {'title': title, 'date': date, 'location': location, 'text': text}
+        notes.append(note)
+        #print note
+    
+except:
+    print 'Unable parse clipping'
+
+# TODO:
+# - before find, first search for tags, then do find on title
+# - try to see if content already exists, to avoid duplicate entries in a note
+def MakeEvernoteNote(note):
+    cmd = '''
+    osascript<<END
+        tell application "Evernote"
+            set note_title to "''' + unicode(note['title'], errors="ignore") + '''"
+            set note_plain_text to "''' + unicode(note['text'].strip(), errors="ignore") + '''"
+            set note_full_contents to "''' + unicode(note['location'], errors="ignore") + unicode(note['date'], errors="ignore") + "\n" + unicode(note['text'], errors="ignore") + "\n" '''"
+            set tag_name to "kindle-note"
+            set note_search_term to note_title
+            set found_notes to find notes note_search_term
+            set num_notes_found to count found_notes
+
+            if num_notes_found is greater than 0 then
+                set this_note to item 1 in found_notes
+                set this_note_text to (HTML content of item 1 of found_notes)
+                if note_plain_text is not in this_note_text then
+                    tell this_note to append text note_full_contents
+                end if
+            else
+                set clip to create note title note_title with text note_full_contents
+                if (not (tag named tag_name exists)) then
+                    make tag with properties {name:tag_name}
+                end if
+                assign tag tag_name to clip
+            end if
+        end tell
+    END'''
+
+    os.system(cmd)
+
+for note in notes:
+    time.sleep(1)
+    MakeEvernoteNote(note)
